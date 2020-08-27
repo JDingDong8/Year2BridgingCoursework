a63d51cb97e377b405f513f950e51425a8400349
JDingDong8 <jbell8@hotmail.co.uk>
2020-08-27 15:49:42 +0100

    My Django Girls app, first commit

---
+++ Year2BridgingCoursework/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Year2BridgingCoursework project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Year2BridgingCoursework.settings')
+
+application = get_wsgi_application()

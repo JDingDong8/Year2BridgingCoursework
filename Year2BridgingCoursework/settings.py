9ad31ac31ff72d75925f8972a925a8073ddd04aa
JDingDong8 <jbell8@hotmail.co.uk>
2020-09-22 18:52:26 +0100

    created middleware to disable browser caching. No more ctrl+f5!!

--- Year2BridgingCoursework/settings.py
+++ Year2BridgingCoursework/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'blog.middleware.NoCache',
 ]
 
 ROOT_URLCONF = 'Year2BridgingCoursework.urls'
